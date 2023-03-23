e8fc1e3009cd4ded7caefdc1537917ee6bfb2da8
leonardochen01 <leonardochen.lc@gmail.com>
2023-03-23 21:35:54 +0100

    added: email array

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//aggiungo bottone login che consente l'accesso se l'utente accede con email loggato
+
+document.addEventListener("click", function(){
+
+  var email = document.getElementById("email").value;
+  var emailList = ["a@gmail.com, b@gmail.com, c@gmail.com"]
+
+})
